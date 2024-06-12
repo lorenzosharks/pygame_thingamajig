ce2fe2e0737ca66a7c1538e3c60c53578a9e7f73
lorenzosharks <karsonx27@lakesideschool.org>
2024-06-11 22:07:51 -0700

    changed image

--- game.py
+++ game.py
@@ -10,7 +10,9 @@ print(rectangle.attack)
 
 pygame.init()
 
-#player_image = pygame.image.load("")  # Replace "player.png" with the path to your image
+player_image = pygame.image.load("actualskibidi.png")  # Replace "player.png" with the path to your image
+
+smallerimage = pygame.transform.scale(player_image, (50, 50))
 
 # sizing and coordinates
 screenW = 1000
@@ -19,17 +21,16 @@ screenH = 800
 sizeW = 50
 sizeH = 50
 
-x = screenW / 2 - sizeW / 2
-y = screenH / 2 - sizeH / 2
+# x = screenW / 2 - sizeW / 2
+# y = screenH / 2 - sizeH / 2
 
-OGx = screenW / 2 - sizeW / 2
-OGy = screenH / 2 - sizeH / 2
+# OGx = screenW / 2 - sizeW / 2
+# OGy = screenH / 2 - sizeH / 2
 
-# x = screenW // 2 - player_image.get_width() // 2
-# y = screenH // 2 - player_image.get_height() // 2
-
-# OGx = screenW // 2 - player_image.get_width() // 2
-# OGy = screenH // 2 - player_image.get_height() // 2
+x = screenW // 2 - smallerimage.get_width() // 2
+y = screenH // 2 - smallerimage.get_height() // 2
+OGx = screenW // 2 - smallerimage.get_width() // 2
+OGy = screenH // 2 - smallerimage.get_height() // 2
 
 health = screenW - 80
 
@@ -48,51 +49,51 @@ allowA = True
 allowD = True
 
 while run:
+    
     # keys pressing thingamajig
     key = pygame.key.get_pressed()
     if allowW:
         if key[pygame.K_w]:
-            y -= 5
+            y = y-5
     if allowS:
         if key[pygame.K_s]:
-            y += 5
+            y = y+5
     if allowA:
         if key[pygame.K_a]:
-            x -= 5
+            x = x-5
     if allowD:
         if key[pygame.K_d]:
-            x += 5
+            x = x-5
     if key[pygame.K_r]:
         x = OGx
         y = OGy
     if allowed:
         if key[pygame.K_l]:
-            health -= 5
+            health = health-5
         if health < screenW - 80:
             if key[pygame.K_p]:
-                health += 5
+                health = health+5
 
     if health < 0:
         health = 0
         allowed = False
 
     screen.fill("green")
-
-    player = pygame.draw.rect(screen, "purple", (x, y, sizeW, sizeH))
     
+    #player = pygame.draw.rect(screen, "purple", (x, y, sizeW, sizeH))
+    screen.blit(smallerimage, (x, y))
     playerC1 = pygame.draw.rect(screen, "green", (x, y, 1, 1))
     playerC3 = pygame.draw.rect(screen, "purple", (x + sizeW, y + sizeH, 1, 1))
 
-    # Update collision detection
+    # update collision detection
     allowA, allowW, allowD, allowS = border_control(playerC1, playerC3, screenW, screenH)
 
-    # health bar
+    # health bar stuff
     healthbar = pygame.draw.rect(screen, "black", (35, 5, screenW - 70, 60))
     pygame.draw.rect(screen, "red", (40, 10, health, 50))
-
     font = pygame.font.Font(None, 36)
-    text_surface = font.render(f"Health: {round(health / ((screenW - 80) / 100), 1)}", True, (255, 255, 255))
-    screen.blit(text_surface, (screenW / 2 - text_surface.get_width() / 2, 20))
+    text_surface = font.render(f"Skibidi's Health: {round(health/((screenW - 80)/100), 1)}", True, (255, 255, 255))
+    screen.blit(text_surface, (screenW/2 - text_surface.get_width()/2, 20))
 
     # makes sure things are displayed well
     pygame.display.update()
