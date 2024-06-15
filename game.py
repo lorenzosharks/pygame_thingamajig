3a6ce72acdfe91ba094b5db4447825814eff899a
lorenzosharks <karsonx27@lakesideschool.org>
2024-06-15 00:27:52 -0700

    lots of stuff (tank related)

--- game.py
+++ game.py
@@ -1,14 +1,9 @@
 # main.py
-
 import pygame
 from character import Character
-from bordercontrol import border_control
 import math
 
-
 rectangle = Character(100, 0, 0, 0, 0, 0)
-
-tank_speed = 1
 
 print(rectangle.attack)
 
@@ -16,7 +11,7 @@ pygame.init()
 
 player_image = pygame.image.load("tank.png")  # Replace "player.png" with the path to your image
 
-smallerimage = pygame.transform.scale(player_image, (50, 50))
+player_image = pygame.transform.scale(player_image, (50, 50))
 
 # sizing and coordinates
 screenW = 1000
@@ -25,10 +20,10 @@ screenH = 800
 sizeW = 50
 sizeH = 50
 
-x = screenW // 2 - smallerimage.get_width() // 2
-y = screenH // 2 - smallerimage.get_height() // 2
-OGx = screenW // 2 - smallerimage.get_width() // 2
-OGy = screenH // 2 - smallerimage.get_height() // 2
+x = screenW // 2 - player_image.get_width() // 2
+y = screenH // 2 - player_image.get_height() // 2
+OGx = screenW // 2 - player_image.get_width() // 2
+OGy = screenH // 2 - player_image.get_height() // 2
 
 health = screenW - 80
 
@@ -48,53 +43,93 @@ allowD = True
 
 # Initialize rotation angle
 angle = 0
+angle_radians = 0
+
+# miscellneos (ie cheats)
+actual_tankspeed = 5
+tank_speed = 0 # dont change this fella
+rotation_speed = 5
+backward_speed = 1
 
 while run:
     
+    screen.fill("green")
+
     key = pygame.key.get_pressed()    
-    if key[pygame.K_e]:
-        angle -= 1  # Rotate counter-clockwise
-    if key[pygame.K_q]:
-        angle += 1  # Rotate clockwis
+    
+    if key[pygame.K_d]:
+        angle -= rotation_speed  # Rotate counter-clockwise
+        angle_radians -= math.radians(rotation_speed)
+
+    if key[pygame.K_a]:
+        angle += rotation_speed
+        angle_radians += math.radians(rotation_speed)
+
     if allowW and key[pygame.K_w]:
-        y -= tank_speed
+        tank_speed = actual_tankspeed
+        y -= tank_speed*math.cos(angle_radians)
+        x -= tank_speed*math.sin(angle_radians)
+    else:
+        tank_speed = 0
+
     if allowS and key[pygame.K_s]:
-        y += tank_speed
-    if allowA and key[pygame.K_a]:
-        x -= tank_speed
-    if allowD and key[pygame.K_d]:
-        x += tank_speed
+        tank_speed = backward_speed
+        y += backward_speed*math.cos(angle_radians)
+        x += backward_speed*math.sin(angle_radians)
+    else:
+        tank_speed = 0
+
     if key[pygame.K_r]:
         x = OGx
         y = OGy
+        angle_radians = 0
+        angle = 0
+
+    if health < 0:
+        health = 0
+        allowed = False
+    
     if allowed:
         if key[pygame.K_l]:
             health -= 5
         if health < screenW - 80:
             if key[pygame.K_p]:
                 health += 5
-
-    if health < 0:
-        health = 0
-        allowed = False
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+                
+    # if allowW and key[pygame.K_w]:
+    #     y -= tank_speed
+    
+    # if allowS and key[pygame.K_s]:
+    #     y += tank_speed
+    
+    # if allowA and key[pygame.K_a]:
+    #     x -= tank_speed
+    
+    # if allowD and key[pygame.K_d]:
+    #     x += tank_speed
+    
+    playerC1 = pygame.draw.rect(screen, "purple", (x, y, 1, 1))
+    playerC2 = pygame.draw.rect(screen, "purple", (x + sizeW, y, 1, 1))
+    playerC3 = pygame.draw.rect(screen, "purple", (x + sizeW, y + sizeH, 1, 1))
+    playerC4 = pygame.draw.rect(screen, "purple", (x, y + sizeH, 1, 1))
+    
+    # Update collision detection
+    if x < 0:
+        x = 0
+    elif x > screenW - sizeW:
+        x = screenW - sizeW
+    if y < 0:
+        y = 0
+    elif y > screenH - sizeH:
+        y = screenH - sizeH
 
     # Rotate the image
-    rotated_image = pygame.transform.rotate(smallerimage, angle)
+    rotated_image = pygame.transform.rotate(player_image, angle)
     rotated_rect = rotated_image.get_rect(center=(x + sizeW // 2, y + sizeH // 2))
 
-    screen.fill("green")
 
     # Draw the rotated image
     screen.blit(rotated_image, rotated_rect.topleft)
-    playerC1 = pygame.draw.rect(screen, "green", (x, y, 1, 1))
-    playerC3 = pygame.draw.rect(screen, "purple", (x + sizeW, y + sizeH, 1, 1))
-
-    # Update collision detection
-    allowA, allowW, allowD, allowS = border_control(playerC1, playerC3, screenW, screenH)
 
     # Health bar stuff
     healthbar = pygame.draw.rect(screen, "black", (35, 5, screenW - 70, 60))
@@ -106,5 +141,10 @@ while run:
     # Make sure things are displayed well
     pygame.display.update()
     dt = clock.tick(60) / 1000
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
 
 pygame.quit()
--- test.py
+++ test.py
@@ -1,43 +1,38 @@
-# Example file showing a circle moving on screen
 import pygame
 
-# pygame setup
+# Initialize Pygame
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
+
+# Set up the display
+width, height = 800, 600
+screen = pygame.display.set_mode((width, height))
+
+# Colors
+green = pygame.Surface((10, 10))
+green.fill((0, 255, 0))
+
+purple = pygame.Surface((10, 10))
+purple.fill((128, 0, 128))
+
+# Coordinates
+x, y = 100, 100
+sizeW, sizeH = 50, 50
+
+# Main loop
 running = True
-dt = 0
-
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    # Fill the background with white
+    screen.fill((255, 255, 255))
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
+    # Blit the colored rectangles (1x1 pixel)
+    screen.blit(green, (x, y))
+    screen.blit(purple, (x + sizeW, y + sizeH))
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
-
-    # flip() the display to put your work on screen
+    # Update the display
     pygame.display.flip()
-
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
-    dt = clock.tick(60) / 1000
 
 pygame.quit()
